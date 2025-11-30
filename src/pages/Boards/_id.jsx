8440da6f002f7691193388fdd8b3197e387da297
LoiTang <heobovidai@gmail.com>
2025-11-30 10:25:32 +0700

    mongodb-aggregate

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,7 +2,7 @@ import Container from '@mui/material/Container';
 import AppBar from '@/components/AppBar/AppBar';
 import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
-import { mockData } from '@/apis/mock-datas';
+// import { mockData } from '@/apis/mock-datas';
 import { useEffect, useState } from 'react';
 // import { useParams } from 'react-router-dom';
 import { fetchBoardDetailsAPI } from '@/apis';
@@ -15,7 +15,7 @@ function Board() {
     //https://react.dev/reference/react/useEffect
     useEffect(() => {
         //lay id tu url bang react-router-dom
-        const boardId = '692ab0de81488e1b24281b37';
+        const boardId = '692b979c341124939784ba36';
 
         //call api
         // du lieu API tra ve la 1 board
